edea1e1f8a3d145093b9a6cf87554663b566d3a3
ShooterCrow <cydollarsign1@gmail.com>
2026-01-31 14:15:43 +0100

    first commit

---
+++ scripts/header.js
@@ -0,0 +1,31 @@
+window.onload = function () {
+  const hambuger = document.querySelector(".hambuger");
+  console.log(hambuger);
+  const mobileNav = document.querySelector(".mobile-nav");
+  const navTheme = document.querySelector(".nav-theme");
+  const themeIcon = document.querySelector(".theme-icon");
+
+  let hamburgerState = false;
+  hambuger.addEventListener("click", () => {
+    mobileNav.classList.toggle("show");
+    if (hamburgerState === false) {
+      hambuger.innerHTML = "❌";
+      hamburgerState = true;
+      return;
+    }
+    if (hamburgerState === true) {
+      hambuger.innerHTML = `<div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>`;
+      hamburgerState = false;
+      return;
+    }
+  });
+
+  function toggleTheme(type) {
+    console.log(type.value);
+  }
+
+  navTheme.addEventListener("change", () => toggleTheme(navTheme));
+  themeIcon.addEventListener("change", () => toggleTheme(themeIcon));
+};
